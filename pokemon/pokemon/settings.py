1a2a7a9037fdca0f2a26ba4b45083f90093ff9f7
Flave <flaroux72@gmail.com>
2021-07-08 18:35:25 +0200

    Add front build

--- pokemon/pokemon/settings.py
+++ pokemon/pokemon/settings.py
@@ -134,7 +134,6 @@ STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "../frontend/src/assets"),
     os.path.join(BASE_DIR, "../frontend/dist"),
 ]
 
