c195d4361c898446894a5378c136f6512781a871
Flave <flaroux72@gmail.com>
2021-07-09 13:56:37 +0200

    Add front Sentry

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,19 @@
+import Vue from "vue";
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import * as Sentry from "@sentry/vue";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    Vue: Vue,
+    dsn: "https://f0bf91902d4a47c7b18660310b8c513d@o481806.ingest.sentry.io/5851254",
+    integrations: [new Integrations.BrowserTracing()],
+
+    // We recommend adjusting this value in production, or using tracesSampler
+    // for finer control
+    tracesSampleRate: 1.0,
+});
 
 // Create pokemon app
 const app = createApp(App)
